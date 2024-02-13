6cf2c8522a0f49045cf70b13e6d07b18cc2d7beb
Valentyn Korniienko <valik.kornienko97@gmail.com>
2024-02-14 01:51:15 +0200

    Update versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.36.0"
+      version = "5.36.0"
     }
     postgresql = {
       source  = "cyrilgdn/postgresql"
-      version = "~> 1.21.0"
+      version = "1.21.0"
     }
   }
 
