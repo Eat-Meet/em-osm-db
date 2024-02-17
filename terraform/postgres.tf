5c0644cfdf656bbb60aba93cf0a5f96f5194e60d
Valentyn Korniienko <valik.kornienko97@gmail.com>
2024-02-17 13:55:52 +0200

    add

--- terraform/postgres.tf
+++ terraform/postgres.tf
@@ -21,19 +21,19 @@ resource "postgresql_extension" "hstore_extension" {
   name = "hstore"
 }
 
-#resource "terraform_data" "download_osm_data" {
-#  provisioner "local-exec" {
-#    working_dir = "../database/"
-#    interpreter = ["/bin/bash", "-c"]
-#    command     = <<-EOT
-#      chmod +x download-osm-ukraine.sh
-#      ./download-osm-ukraine.sh
-#    EOT
-#    environment = {
-#      DB_NAME     = nonsensitive(aws_db_instance.osm_rds.identifier)
-#      DB_ADDRESS  = nonsensitive(aws_db_instance.osm_rds.address)
-#      DB_USERNAME = nonsensitive(aws_db_instance.osm_rds.username)
-#      DB_PASSWORD = nonsensitive(aws_db_instance.osm_rds.password)
-#    }
-#  }
-#}
+resource "terraform_data" "download_osm_data" {
+  provisioner "local-exec" {
+    working_dir = "../database/"
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<-EOT
+      chmod +x download-osm-ukraine.sh
+      ./download-osm-ukraine.sh
+    EOT
+    environment = {
+      DB_NAME     = nonsensitive(aws_db_instance.osm_rds.identifier)
+      DB_ADDRESS  = nonsensitive(aws_db_instance.osm_rds.address)
+      DB_USERNAME = nonsensitive(aws_db_instance.osm_rds.username)
+      DB_PASSWORD = nonsensitive(aws_db_instance.osm_rds.password)
+    }
+  }
+}
