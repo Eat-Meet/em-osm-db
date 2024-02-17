2c0f71ac71089fb3ac8e2915245514d02b36e3b6
Valentyn Korniienko <valik.kornienko97@gmail.com>
2024-02-17 14:02:20 +0200

    remove

--- database/download-osm-ukraine.sh
+++ database/download-osm-ukraine.sh
@@ -8,12 +8,5 @@ wget https://download.openstreetmap.fr/extracts/europe/ukraine/lviv_oblast-lates
 echo "Download OSM data for Lviv region is completed."
 
 echo "Run OSM Migration."
-sudo osm2pgsql -s -U $DB_USERNAME -W $DB_PASSWORD -l -d $DB_NAME -H $DB_ADDRESS --hstore lviv_oblast-latest.osm.pbf
+sudo osm2pgsql -s -U $DB_USERNAME -l -d $DB_NAME -H $DB_ADDRESS --hstore lviv_oblast-latest.osm.pbf
 echo "OSM Migration is completed."
-
-if [ $? -eq 0 ]; then
-    echo "OSM migration completed successfully."
-else
-    echo "OSM migration failed." >&2
-    exit 1
-fi
